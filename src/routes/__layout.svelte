5f89c53a3ad619f26f3fcbae6effa9853663ccb7
Marten <marten.frisk@gmail.com>
2021-05-17 18:16:21 +0200

    Add sidebar to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
 	import '../app.css';
+	import Sidebar from '$lib/Sidebar/index.svelte';
 
 </script>
 
-<slot />
+<!-- <div class="w-full flex flex-col items-center my-4">
+	<h1 class="text-3xl text-blue-700">Handbook Search</h1>
+	<p>The unofficial Hollywood Handbook search engine</p>
+</div> -->
+<div class="flex">
+
+	<Sidebar />
+	<main class="w-full md:w-3/4">
+		<slot />
+	</main>
+</div>
